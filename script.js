f4e317324222c0d9d4e07d888736ce1c9bc57dc7
Toty-01 <caucheanthony@gmail.com>
2022-10-27 02:39:32 +0200

    grid invaders

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ class Player {
     this.rotation = 0
 
     const image = new Image()
-    image.src = './img/spaceship.png'
+    image.src = './img/spaces.png'
     image.onload = () => {
       const scale = 0.15
       this.image = image
@@ -36,7 +36,6 @@ class Player {
     player.position.x + player.width / 2, 
     player.position.y + player.height / 2
     )
-  
    c.rotate(this.rotation)
 
    c.translate(
@@ -53,6 +52,7 @@ class Player {
     )
     c.restore()
   }
+
   update() {  
    if (this.image) {
     this.draw()
@@ -65,25 +65,71 @@ class Projectile {
   constructor({position, velocity}) {
     this.position = position
     this.velocity = velocity
-    this.radius = 3 
+    this.radius = 4
   }
 
   draw() {
     c.beginPath()
-    c.arc(this.position.x, this.position.y, this. radius, 0, Math.PI * 2)
+    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
     c.fillStyle ='blue'
     c.fill()
     c.closePath()
   }
+
+
   update() {
     this.draw()
     this.position.x += this.velocity.x
     this.position.y += this.velocity.y
   }
 }
+class Invader {
+  constructor() {
+    this.velocity = {
+      x: 0,
+      y: 0
+    }
+
+    const image = new Image()
+    image.src = './img/invader.png'
+    image.onload = () => {
+      const scale = 1
+      this.image = image
+      this.width = image.width * scale
+      this.height = image.height * scale
+      this.position = {
+        x: canvas.width / 2 - this.width / 2,
+        y: canvas.height / 2
+      }
+    }
+  }
+
+  draw() {
+   // c.fillStyle = 'red'
+   // c.fillRect(this.position.x, this.position.y, this.width, this.height)
+  
+   
+   c.drawImage(
+      this.image, 
+      this.position.x, 
+      this.position.y, 
+      this.width, 
+      this.height
+    )
+  }
+
+  update() {  
+   if (this.image) {
+    this.draw()
+    this.position.x += this.velocity.x
+    this.position.y += this.velocity.y
+    }
+  }
+}
 
 const player = new Player()
 const projectiles = []
+const invader = new Invader()
 const keys = {
   ArrowLeft: {
     pressed: false
@@ -100,6 +146,7 @@ function animate() {
   requestAnimationFrame(animate)
   c.fillStyle = 'black'
   c.fillRect(0, 0, canvas.width, canvas.height)
+  invader.update()
   player.update()
   projectiles.forEach((projectile, index) => {
     if (projectile.position.y + projectile.radius <= 0) {
@@ -114,8 +161,8 @@ function animate() {
   if (keys.ArrowLeft.pressed && player.position.x >= 0) {
     player.velocity.x = -5
     player.rotation = -0.15
-   } else if (keys.ArrowRight.pressed && player.position.x +player.width <= canvas.width) {
-    player.velocity.x = 5
+   } else if (keys.ArrowRight.pressed && player.position.x + player.width <= canvas.width) {
+    player.velocity.x = +5
     player.rotation = 0.15
   } else {
     player.velocity.x = 0
